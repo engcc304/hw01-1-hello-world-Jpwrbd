a8ff0108ad22ac59ebfc215f405025bd14045275
Papawarin Bussadee <119738757+Jpwrbd@users.noreply.github.com>
2023-06-29 16:09:12 +0700

    Update hw01-1-helloworld.cpp

--- Homework/HW01/hw01-1-helloworld.cpp
+++ Homework/HW01/hw01-1-helloworld.cpp
@@ -1,10 +1,3 @@
 //TODO: #3 provide me full syntax of C-Language contain include, main function(int).
-#include <stdio.h>
-  int main() {}
 //TODO: #4 display me "Hello World." to me.
-#include <stdio.h>
-  int main() {
-    printf("Hello world");
-    return 0;
-  }
 //TODO: #5 Write your private info into Student-Info.txt
